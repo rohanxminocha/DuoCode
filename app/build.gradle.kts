893bff351494bb077c9733aad6118a9ccbe80e9c
Raiyan Sayeed <raiyan.business@gmail.com>
2025-04-04 00:33:42 -0400

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
